8a70db65f60390c175612ee974af87d3735177ec
Tobias Tengler <45513122+tobias-tengler@users.noreply.github.com>
2019-09-07 15:35:51 +0200

    Add FtpResponse

---
+++ Classes/FtpResponse.cs
@@ -0,0 +1,28 @@
+namespace Ftp.Net
+{
+    public class FtpResponse
+    {
+        public FtpResponse()
+        {
+
+        }
+
+        public FtpResponse(uint code)
+        {
+            Code = code;
+        }
+
+        public FtpResponse(uint code, string message) : this(code)
+        {
+            Message = message;
+        }
+
+        public uint Code { get; set; }
+
+        public string Message { get; set; }
+
+        public string Data { get; set; }
+
+        public bool Success => Code >= 100 && Code < 400;
+    }
+}
